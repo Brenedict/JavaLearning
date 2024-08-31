e3330f9209cde770bc69f6d42ff7908401658b74
Benedithos <johnsjbinas@gmail.com>
2024-08-31 13:10:08 +0800

    Start the barebones
    Added 2 methods to the databaseUtil class.

--- SchoolManager/src/Main.java
+++ SchoolManager/src/Main.java
@@ -1,8 +1,55 @@
-
+import java.sql.SQLException;
+import java.util.Scanner;
 public class Main {
+    static databaseUtil database = new databaseUtil();
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        databaseUtil database = new databaseUtil();
+        int menuConf = 0;
         database.connect();
-        database.insertRow();
+        do {
+            System.out.println("LeetCode Log (Benedict)");
+            System.out.println(" (1) Display All Logs");
+            System.out.println(" (2) Insert New Log\n");
+
+            System.out.print("> ");
+            menuConf = input.nextInt();
+
+            switch(menuConf) {
+                case 1:
+                    database.displayData();
+                    break;
+                case 2:
+                    newEntry();
+                    break;
+                default:
+                    System.out.println("\n\n!!Input off-range!!\n");
+            }
+        } while(true);
+
+
+//        database.insertNewRow(4018, "Medium", "Correct", "Random Bull");
+
+
+    }
+
+    public static void newEntry() throws SQLException {
+        int ID;
+        String difficulty, attempt, details;
+
+        System.out.print("Enter Question ID No. #");
+        ID = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter Question Difficulty (Easy-Medium-Hard): ");
+        difficulty = input.nextLine();
+
+        System.out.print("Attempt (Correct/Wrong): ");
+        attempt = input.nextLine();
+
+        System.out.println("Enter Question Description:");
+        details = input.nextLine();
+
+        database.insertNewRow(ID, difficulty, attempt, details);
     }
 }
--- SchoolManager/src/databaseUtil.java
+++ SchoolManager/src/databaseUtil.java
@@ -1,26 +1,51 @@
 import java.sql.*;
 
 public class databaseUtil {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/schooldata"; // Replace with your database name
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/leettracker"; // Replace with your database name
     private static final String DB_USERNAME = "root"; // Replace with your MySQL username
     private static final String DB_PASSWORD = "Benedict#28"; // Replace with your MySQL password
     private Connection connection = null;
 
-
     public void connect() throws SQLException, Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection(JDBC_URL, DB_USERNAME, DB_PASSWORD);
+        System.out.println(connection = DriverManager.getConnection(JDBC_URL, DB_USERNAME, DB_PASSWORD));
         System.out.println("Connection established...");
-
-
     }
 
-    public void insertRow() throws SQLException {
+    public void displayData() throws SQLException {
         Statement statement = connection.createStatement();
-        statement.execute("INSERT INTO student_tracker VALUES(1, 'John Benedict', 'Binas');");
+        ResultSet result = statement.executeQuery("Select * FROM leet_tracker_sheet;");
+        System.out.println("\n================================================================\n");
+        System.out.println("Question ID\t\tDifficulty\t\tAttempt\t\t\t   Date");
+        while (result.next()) {
+            int ID = result.getInt(1);
+            String attempt = result.getString(2);
+            String difficulty = result.getString(3);
+            String attemptDate = result.getString(4);
+            String details = result.getString(5);
+            System.out.printf("   %4d\t\t\t%s\t\t\t%s\t\t\t%s\n", ID, attempt, difficulty, attemptDate);
+        }
+        System.out.println("\n================================================================\n");
+        statement.close();
+        result.close();
     }
 
+    public void insertNewRow(int ID, String difficulty, String attempt, String details) throws SQLException{
+        PreparedStatement prepStatement = connection.prepareStatement("INSERT INTO leet_tracker_sheet VALUES(?, ?, ?, CURRENT_DATE(), ?);");
+        prepStatement.setInt(1, ID);
+        prepStatement.setString(2, difficulty);
+        prepStatement.setString(3, attempt);
+        prepStatement.setString(4, details);
 
+        prepStatement.executeUpdate();
+        System.out.printf("Row successfully added ID#%d\n", ID);
+        prepStatement.close();
+    }
+
+    public void disconnect() throws SQLException {
+        connection.close();
+        System.out.println("Connection successfully disconnected...");
+    }
 
 }
 
