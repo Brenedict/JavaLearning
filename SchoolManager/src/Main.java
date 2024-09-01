6aece8d11348fc87f56c0a7937b20ae802d46f76
Benedithos <johnsjbinas@gmail.com>
2024-09-01 22:18:26 +0800

    Lazy work

--- SchoolManager/src/Main.java
+++ SchoolManager/src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         do {
             System.out.println("LeetCode Log (Benedict)");
             System.out.println(" (1) Display All Logs");
-            System.out.println(" (2) Insert New Log\n");
-
-            System.out.print("> ");
+            System.out.println(" (2) Insert New Log");
+            System.out.println(" (3) Delete Existing Log");
+            System.out.print("\n> ");
             menuConf = input.nextInt();
 
             switch(menuConf) {
@@ -58,16 +58,38 @@ public class Main {
 
     public static void deleteEntry() throws SQLException {
         int ID;
+        String conf = "Y";
         do {
             try {
                 System.out.print("Search by Id: ");
                 ID = input.nextInt();
+                input.nextLine();
+                if(!database.exist(ID)) {
+                    System.out.println("ID doesn't exist!");
+                    continue;
+                }
 
+                database.displayDataByID(ID);
+                System.out.print("\n\nAre you sure to delete this log (Y/N) or (Q to quit):\n\n> ");
+                conf = input.nextLine();
+
+                switch (conf.toUpperCase()) {
+                    case "N":
+                        break;
+                    case "Y":
+                        database.deleteLog(ID);
+                        System.out.printf("\n\nID #%d successfully deleted from the DataBase!\n", ID);
+                        break;
+                    case "Q":
+                        break;
+                    default:
+                        System.out.println("\n\n!!Not an option!!\n");
+                }
             }
             catch(Exception e) {
                 input.nextLine();
                 System.out.println("\n\n!!Input not an integer!!\n");
             }
-        } while(true);
+        } while(!conf.equals("Q"));
     }
 }
--- SchoolManager/src/databaseUtil.java
+++ SchoolManager/src/databaseUtil.java
@@ -1,9 +1,11 @@
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class databaseUtil {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/leettracker"; // Replace with your database name
     private static final String DB_USERNAME = "root"; // Replace with your MySQL username
     private static final String DB_PASSWORD = "Benedict#28"; // Replace with your MySQL password
+    private static final String DB_NAME = "leet_tracker_sheet";
     private Connection connection = null;
 
     public void connect() throws SQLException, Exception {
@@ -13,25 +15,39 @@ public class databaseUtil {
     }
 
     public void displayData() throws SQLException {
+        String sql = "Select * FROM " + DB_NAME +";";
         Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery("Select * FROM leet_tracker_sheet;");
+        ResultSet result = statement.executeQuery(sql);
         System.out.println("\n================================================================\n");
         System.out.println("Question ID\t\tDifficulty\t\tAttempt\t\t\t   Date");
         while (result.next()) {
-            int ID = result.getInt(1);
-            String attempt = result.getString(2);
-            String difficulty = result.getString(3);
-            String attemptDate = result.getString(4);
-            String details = result.getString(5);
-            System.out.printf("   %4d\t\t\t%s\t\t\t%s\t\t\t%s\n", ID, attempt, difficulty, attemptDate);
+            printAllColumnValues(result);
         }
         System.out.println("\n================================================================\n");
         statement.close();
         result.close();
     }
 
+    public void displayDataByID(int ID) throws SQLException {
+        String sql = "SELECT * FROM " + DB_NAME + " WHERE ID = ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, ID);
+        ResultSet result = preparedStatement.executeQuery();
+        if(result.next()) {
+            printAllColumnValues(result);
+        }
+    }
+    private void printAllColumnValues(ResultSet result) throws SQLException {
+        int ID = result.getInt(1);
+        String attempt = result.getString(2);
+        String difficulty = result.getString(3);
+        String attemptDate = result.getString(4);
+        String details = result.getString(5);
+        System.out.printf("   %4d\t\t\t%s\t\t\t%s\t\t\t%s\n", ID, attempt, difficulty, attemptDate);
+    }
     public void insertNewRow(int ID, String difficulty, String attempt, String details) throws SQLException{
-        PreparedStatement prepStatement = connection.prepareStatement("INSERT INTO leet_tracker_sheet VALUES(?, ?, ?, CURRENT_DATE(), ?);");
+        String sql = "INSERT INTO " + DB_NAME + " VALUES(?, ?, ?, CURRENT_DATE(), ?);";
+        PreparedStatement prepStatement = connection.prepareStatement(sql);
         prepStatement.setInt(1, ID);
         prepStatement.setString(2, difficulty);
         prepStatement.setString(3, attempt);
@@ -41,6 +57,28 @@ public class databaseUtil {
         System.out.printf("Row successfully added ID#%d\n", ID);
         prepStatement.close();
     }
+
+    public void deleteLog(int ID) throws SQLException {
+        String sql = "DELETE FROM " + DB_NAME + " WHERE ID = ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, ID);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
+    public boolean exist(int ID) throws SQLException {
+        String sql = "SELECT * FROM " + DB_NAME + " WHERE ID = ?";
+        PreparedStatement prepStatement = connection.prepareStatement(sql);
+        prepStatement.setInt(1, ID);
+        ResultSet result = prepStatement.executeQuery();
+
+        while(result.next()) {
+            if(result.getInt(1) != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     public void disconnect() throws SQLException {
         connection.close();
